68e12463f3f0832ad2c4b8e58a797aaaf2231307
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-14 12:47:45 -0800

    delete todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,12 @@
 // check off specific todos by clicking
 $("li").click(function() {
 	$(this).toggleClass("completed");
+});
 
-	// //if li is gray
-	// if ($(this).css("color") === "rgb(128, 128, 128)") {
-	// 	// turn it black
-	// 	$(this).css({
-	// 		color: "black",
-	// 		textDecoration: "none"
-	// 	});
-	// } else {
-	// 	$(this).css({
-	// 		color: "gray",
-	// 		textDecoration: "line-through"
-	// 	});
-	// } 
+// click on x to delete todo
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
