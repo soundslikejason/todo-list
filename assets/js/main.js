b63a6456274844a9af81e175469f49a8aa12e7df
Jason Leong <soundslikejason@users.noreply.github.com>
2017-01-14 13:10:14 -0800

    create new todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
 // check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 // click on x to delete todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) {
+		// grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// create new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
